b170bd6905a5d3ed6434a73d33247c3b52027dc2
SadguruSai <mc23bt011@iitdh.ac.in>
2026-05-15 19:25:17 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        unordered_map<char,int> hash;
+        for(auto i:s){
+            hash[i]++;
+        }
+        for(auto i:t){
+            if(hash[i]==0){
+                return false;
+            }
+            hash[i]--;
+        }
+        return true;
+    }
+};
