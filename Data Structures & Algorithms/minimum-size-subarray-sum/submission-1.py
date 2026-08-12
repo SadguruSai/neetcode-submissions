233e06615f65da88e2339b54f16b4d350c9ca5df
SadguruSai <mc23bt011@iitdh.ac.in>
2026-08-12 22:47:55 +0530

    Add: minimum-size-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        #sliding window problem
+        #keep adding right until sum becomes greater than given target
+        #once become greater remove from left keep ans=min(ans,new)
+        #keep doing until found min
+        left,right=0,0
+        ans=float('inf')
+        curr_sum=0
+        while(right<len(nums)):
+            curr_sum+=nums[right]
+            while(curr_sum>=target):
+                ans=min(ans,right-left+1)
+                curr_sum -= nums[left]
+                left+=1
+            right+=1
+        return 0 if ans==float('inf') else ans
