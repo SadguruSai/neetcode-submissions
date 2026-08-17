3b7dcb6b59ab8bef5e6a55719acf6e1c5991a9dd
SadguruSai <mc23bt011@iitdh.ac.in>
2026-08-17 21:58:50 +0530

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res=""
+        resLen=0
+        def checkpali(l,r):
+            while(l>=0 and r<len(s) and s[l]==s[r]):
+                nonlocal res,resLen
+                if(r-l+1>resLen):
+                    res=s[l:r+1]
+                    resLen=r-l+1
+                l-=1
+                r+=1
+        for i in range(len(s)):
+            l,r=i,i
+            checkpali(l,r)
+            l,r=i,i+1
+            checkpali(l,r)
+        return res
